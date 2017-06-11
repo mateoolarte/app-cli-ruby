71a79d772c45d587ede7654e368c58da8aee7497
Mateo Olarte <mateo.olarte8@gmail.com>
2017-06-11 17:47:54 -0500

    version 2 proyecto ruby

--- reto5.rb
+++ reto5.rb
@@ -4,50 +4,84 @@ def welcome_message
   puts
 end
 
-aq = [
-  {
-    question: "Cuanto es 2 + 2",
-    answer: 4
-  },
-  {
-    question: "Cual es la capital de Colombia",
-    answer: "Bogota"
-  },
-  {
-    question: "Cuantos huesos tiene el cuerpo humano",
-    answer: 206
-  },
-  {
-    question: "Cuantas horas tiene un día",
-    answer: 24
-  },
-  {
-    question: "¿Cual es la empresa de producción de carros electricos más grande del mundo?",
-    answer: "Tesla"
-  }
-]
+def questionsanswers
+  hash = File.foreach("aq.txt").map do |line|
+    line.strip
+  end.delete_if { |line| line == "" }
+
+  hash = Hash[*hash]
+end
 
 welcome_message
 
-aq.each_with_index do |element, index|
-  print "Definición: "
-  puts aq[index][:question]
 
+questionsanswers.each do |element|
+  print "Definición: "
+  puts element[0]
   answer = ""
 
-  until aq[index][:answer] == answer
+  until element[1] == answer
     print "Adivinar: "
 
-    if aq[index][:answer].is_a? Integer
-      answer = gets.chomp.to_i
-    else
-      answer = gets.chomp.capitalize!
-    end
+    answer = gets.chomp
 
-    if aq[index][:answer] == answer
+    if element[1] == answer
       puts "Correcto!"
     else
       puts "Incorrecto!, Trata de nuevo"
     end
   end
 end
+
+puts "-"*50
+
+puts "Felicitaciones has finalizado el juego"
+
+############# V1 #############
+# aq = [
+#   {
+#     question: "Cuanto es 2 + 2",
+#     answer: 4
+#   },
+#   {
+#     question: "Cual es la capital de Colombia",
+#     answer: "Bogota"
+#   },
+#   {
+#     question: "Cuantos huesos tiene el cuerpo humano",
+#     answer: 206
+#   },
+#   {
+#     question: "Cuantas horas tiene un día",
+#     answer: 24
+#   },
+#   {
+#     question: "¿Cual es la empresa de producción de carros electricos más grande del mundo?",
+#     answer: "Tesla"
+#   }
+# ]
+#
+# welcome_message
+#
+# aq.each_with_index do |element, index|
+#   print "Definición: "
+#   puts aq[index][:question]
+#
+#   answer = ""
+#
+#   until aq[index][:answer] == answer
+#     print "Adivinar: "
+#
+#     if aq[index][:answer].is_a? Integer
+#       answer = gets.chomp.to_i
+#     else
+#       answer = gets.chomp.capitalize!
+#     end
+#
+#     if aq[index][:answer] == answer
+#       puts "Correcto!"
+#     else
+#       puts "Incorrecto!, Trata de nuevo"
+#     end
+#   end
+# end
